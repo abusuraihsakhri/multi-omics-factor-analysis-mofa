733a96b94e938ddea6c9e1553a4b0a61acb68862
abusuraihsakhri <abusuraihsakhri@gmail.com>
2026-08-26 00:01:52 +0530

    Initial Commit

---
+++ mofa_integrator/__init__.py
@@ -0,0 +1,6 @@
+"""
+MOFA-Integrator: Multi-Omics Factor Analysis & Variance Decomposition Agent
+Domain: Biomedical Scientific Discovery & Multi-Omics
+Standard: Multi-Omics Factor Analysis (MOFA+) Framework
+"""
+__version__ = "2.0.0-FRONTIER"
